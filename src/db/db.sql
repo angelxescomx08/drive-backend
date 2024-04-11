4be7b27ea1348e739bac32f4d27e769cf89bb370
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-04-11 11:55:55 -0600

    campos de nombres en la base de datos

--- src/db/db.sql
+++ src/db/db.sql
@@ -8,6 +8,7 @@ CREATE TABLE "folder" (
   "id_folder" varchar(64) PRIMARY KEY,
   "id_parent" varchar(64),
   "id_user" varchar(64),
+  "folder_name" varchar,
   "created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY ("id_user") REFERENCES "user" ("id_user"),
   FOREIGN KEY ("id_parent") REFERENCES "folder" ("id_folder")
@@ -17,6 +18,7 @@ CREATE TABLE "file" (
   "id_file" varchar(64) PRIMARY KEY,
   "id_folder" varchar(64),
   "updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+  "file_name" varchar,
   "url" varchar,
   FOREIGN KEY ("id_folder") REFERENCES "folder" ("id_folder")
 );
