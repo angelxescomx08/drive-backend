213e0aef2d4f00292ae24dc82c65132e511cf22f
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-04-25 19:52:19 -0600

    schema de la base de datos con drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
 
-const result = dotenv.config({
+const { parsed } = dotenv.config({
   path: "./.env.development",
 });
 
@@ -12,7 +12,7 @@ export default {
   dbCredentials: {
     //url: process.env.URL_DATABASE!,
     //authToken: process.env.DB_AUTH_TOKEN!,
-    url: result.parsed!.URL_DATABASE!,
+    url: parsed!.URL_DATABASE!,
   },
   verbose: true,
 } satisfies Config;
