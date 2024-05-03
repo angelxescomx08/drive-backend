4f264d8e7e4965a4bb35148febc47c216b84f4b9
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-05-03 17:06:47 -0600

    indices

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,33 +1,63 @@
-import { AnySQLiteColumn, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import {
+  AnySQLiteColumn,
+  index,
+  sqliteTable,
+  text,
+  uniqueIndex,
+} from "drizzle-orm/sqlite-core";
 
-export const user = sqliteTable("user", {
-  id_user: text("id_user", { length: 36 }).primaryKey(),
-  email: text("email").unique().notNull(),
-  password: text("password", { length: 60 }).notNull(),
-});
+export const user = sqliteTable(
+  "user",
+  {
+    id_user: text("id_user", { length: 36 }).primaryKey(),
+    email: text("email").unique().notNull(),
+    password: text("password", { length: 60 }).notNull(),
+  },
+  (table) => {
+    return {
+      email_idx: uniqueIndex("email_idx").on(table.email),
+    };
+  }
+);
 
-export const folder = sqliteTable("folder", {
-  id_folder: text("id_folder", { length: 36 }).primaryKey(),
-  id_parent: text("id_parent", { length: 36 }).references(
-    (): AnySQLiteColumn => folder.id_folder,
-    { onDelete: "cascade" }
-  ),
-  id_user: text("id_user", { length: 36 })
-    .references(() => user.id_user, { onDelete: "cascade" })
-    .notNull(),
-  folder_name: text("folder_name").notNull(),
-});
+export const folder = sqliteTable(
+  "folder",
+  {
+    id_folder: text("id_folder", { length: 36 }).primaryKey(),
+    id_parent: text("id_parent", { length: 36 }).references(
+      (): AnySQLiteColumn => folder.id_folder,
+      { onDelete: "cascade" }
+    ),
+    id_user: text("id_user", { length: 36 })
+      .references(() => user.id_user, { onDelete: "cascade" })
+      .notNull(),
+    folder_name: text("folder_name").notNull(),
+  },
+  (table) => {
+    return {
+      id_parent_idx: index("id_parent_idx").on(table.id_parent),
+    };
+  }
+);
 
-export const file = sqliteTable("file", {
-  id_file: text("id_file", { length: 36 }).primaryKey(),
-  id_folder: text("id_folder", { length: 36 }).references(
-    () => folder.id_folder,
-    { onDelete: "cascade" }
-  ),
-  file_name: text("file_name").notNull(),
-  url: text("url").notNull(),
-  aws_key: text("aws_key").notNull(),
-});
+export const file = sqliteTable(
+  "file",
+  {
+    id_file: text("id_file", { length: 36 }).primaryKey(),
+    id_folder: text("id_folder", { length: 36 }).references(
+      () => folder.id_folder,
+      { onDelete: "cascade" }
+    ),
+    file_name: text("file_name").notNull(),
+    url: text("url").notNull(),
+    aws_key: text("aws_key").notNull(),
+  },
+  (table) => {
+    return {
+      id_folder_idx: index("id_folder_idx").on(table.id_folder),
+    };
+  }
+);
 
 export type InsertUser = typeof user.$inferInsert;
 export type InsertFolder = typeof folder.$inferInsert;
