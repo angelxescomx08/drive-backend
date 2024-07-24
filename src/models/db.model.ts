ac1512964d145eb43ccc8353aacd7b28110471ef
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2024-07-23 19:05:41 -0600

    eliminar cosas inutiles

--- src/models/db.model.ts
+++ src/models/db.model.ts
@@ -1,6 +1,4 @@
 import { Client, createClient } from "@libsql/client";
-import { User } from "./user.model";
-import { Folder } from "./folder.model";
 import { LibSQLDatabase } from "drizzle-orm/libsql";
 import { dbDrizzle } from "../db/db";
 import * as schema from "../db/schema";
@@ -8,15 +6,11 @@ import { environment } from "../config/environment";
 
 export class Database {
   private client!: Client;
-  public user!: User;
-  public folder!: Folder;
 
   public dbDrizzle!: LibSQLDatabase<typeof schema>;
 
   constructor() {
     this.createClient();
-    this.user = new User(this.client);
-    this.folder = new Folder(this.client);
     this.dbDrizzle = dbDrizzle;
   }
 
